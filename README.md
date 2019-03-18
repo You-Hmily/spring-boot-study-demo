078588a7014db20b775bbe0ab91a27ae3cb780e6
You-Hmily <706097141@qq.com>
2019-03-18 20:44:58 +0800

    整合myBatis

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ###20190314 SpringBoot 整合JSP。
 ###20190315 SpringBoot 关于嵌入式Web容器---Embedded Tomcat
 ###20190316 SpringBoot 使用Thymelea模板配置 
-###20190317 SpringBoot JDBC 
+###20190317 SpringBoot JDBC  26分钟
+###20190318 SpringBoot Mybatis 57分钟
---
+++ Spring-Boot-Exercise/Spring-Boot-Exercise-Mybatis/src/main/java/com/hmily/springboot/MybatisApplication.java
@@ -0,0 +1,20 @@
+package com.hmily.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zyzhmily on 2019/3/18.
+ */
+
+@SpringBootApplication
+public class MybatisApplication {
+
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class,args);
+    }
+
+}
