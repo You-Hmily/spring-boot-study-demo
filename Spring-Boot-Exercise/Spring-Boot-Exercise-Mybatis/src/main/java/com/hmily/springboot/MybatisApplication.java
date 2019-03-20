3ef04bb5f6708819c8dbbcbf1f80ddc217bb537c
You-Hmily <706097141@qq.com>
2019-03-20 23:50:50 +0800

    整合tk-mybatis

--- Spring-Boot-Exercise/Spring-Boot-Exercise-Mybatis/src/main/java/com/hmily/springboot/MybatisApplication.java
+++ Spring-Boot-Exercise/Spring-Boot-Exercise-Mybatis/src/main/java/com/hmily/springboot/MybatisApplication.java
@@ -2,12 +2,14 @@ package com.hmily.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created by zyzhmily on 2019/3/18.
  */
 
 @SpringBootApplication
+//@MapperScan(basePackages = {"com.hmily.springboot.mybatis.user"})
 public class MybatisApplication {
 
     public static void main(String[] args) {
--- Spring-Boot-Exercise/Spring-Boot-Exercise-Mybatis/src/main/java/com/hmily/springboot/mybatis/user/UserMapper.java
+++ Spring-Boot-Exercise/Spring-Boot-Exercise-Mybatis/src/main/java/com/hmily/springboot/mybatis/user/UserMapper.java
@@ -8,9 +8,7 @@ import org.apache.ibatis.annotations.Select;
 /**
  * Created by zyzhmily on 2019/3/19.
  */
-@Mapper
+//@Mapper
 public interface UserMapper extends BaseMapper<User>{
 
-    @Select("SELECT count(*) as totalRow FROM user")
-    Integer gettotalRow();
 }
