2ca6a0ba6807695b0bdf451dfce8aa5e31d7786a
You-Hmily <706097141@qq.com>
2019-03-22 23:58:24 +0800

    DisconfApplication.java

---
+++ Spring-Boot-Exercise/Spring-Boot-Exercise-Disconf/src/main/java/com/hmily/springboot/DisconfApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.springboot;
+
+/**
+ * Created by zyzhmily on 2019/3/22.
+ */
+@SpringBootApplication
+public class DisconfApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
