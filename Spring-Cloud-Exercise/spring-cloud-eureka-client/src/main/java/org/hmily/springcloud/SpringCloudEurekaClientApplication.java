d3aec224e5254ee7787be78f69b8e6fbe7da84bc
You-Hmily <706097141@qq.com>
2019-11-17 16:37:49 +0800

    springcloud 练习demo 修改

--- Spring-Cloud-Exercise/spring-cloud-eureka-client/src/main/java/org/hmily/springcloud/SpringCloudEurekaClientApplication.java
+++ Spring-Cloud-Exercise/spring-cloud-eureka-client/src/main/java/org/hmily/springcloud/SpringCloudEurekaClientApplication.java
@@ -3,14 +3,15 @@ package org.hmily.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaServerApplication {
+public class SpringCloudEurekaClientApplication {
+
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+        SpringApplication.run(SpringCloudEurekaClientApplication.class,args);
     }
 
 }
