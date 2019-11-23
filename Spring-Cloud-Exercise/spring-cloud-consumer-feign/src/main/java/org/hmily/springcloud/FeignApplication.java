2fec68a1334d31d3b6c03d0eaa99951d88257226
You-Hmily <706097141@qq.com>
2019-11-23 19:15:56 +0800

    spring cloud upload

--- Spring-Cloud-Exercise/spring-cloud-consumer-feign/src/main/java/org/hmily/springcloud/FeignApplication.java
+++ Spring-Cloud-Exercise/spring-cloud-consumer-feign/src/main/java/org/hmily/springcloud/FeignApplication.java
@@ -1,12 +1,21 @@
 package org.hmily.springcloud;
 
 
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @EnableFeignClients
 @EnableDiscoveryClient
